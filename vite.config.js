2dc7a788f7487d0c10a99fb3e10477db9fd23997
Austyn Nguyen <cubish@Cubish-MacTop-2.local>
2025-05-08 15:16:29 -0400

    fixed base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/YOUR_REPOSITORY_NAME",
+  base: "/austyn",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
