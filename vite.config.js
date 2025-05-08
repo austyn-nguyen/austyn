688b7dd68e7246c821d9e9a2aac63699523aefa2
Austyn Nguyen <cubish@Cubish-MacTop-2.local>
2025-05-08 15:15:52 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/YOUR_REPOSITORY_NAME",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
